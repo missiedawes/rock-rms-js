49dec477653d1f16779eb1a9e080c167ccc2ed5c
missiedawes <missiedawes@gmail.com>
2025-07-19 13:50:51 -0400

    Initialize TypeScript project with basic structure and configuration

---
+++ src/client.ts
@@ -0,0 +1,6 @@
+// TypeScript version of RockRMS client
+// Ported from rock_rms/lib/rock_rms/client.rb
+
+export class RockRmsClient {
+    // TODO: Implement client logic here, following btreejs style
+}
