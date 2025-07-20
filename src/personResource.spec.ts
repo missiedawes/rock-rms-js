9cc66023d38603e3cb27f09a3acc77af7b76cb3e
missiedawes <missiedawes@gmail.com>
2025-07-20 19:45:00 +0000

    Refactor devcontainer configuration and add VSCode extensions; update .gitignore and .mocharc.json for improved testing setup; implement PersonResource tests

---
+++ src/personResource.spec.ts
@@ -0,0 +1,15 @@
+import { RockRmsClient } from './client';
+import { expect } from 'chai';
+
+describe('PersonResource', function () {
+    it('should fetch person with ID 358837', async function () {
+        // You may need to set ROCK_RMS_URL and ROCK_RMS_API_KEY in your .env file
+        const client = new RockRmsClient();
+        const person = await client.person().get(358837);
+        expect(person).to.be.an('object');
+        expect(person).to.have.property('id');
+        expect(person.id).to.equal(358837);
+        expect(person).to.have.property('firstName');
+        expect(person).to.have.property('lastName');
+    });
+});
